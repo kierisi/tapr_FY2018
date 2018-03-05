db370e8fb77c1963b7f522f740faa148149a96ea
Jesse Mostipak <jmostipak@teachingtrust.org>
2018-03-05 14:30:14 -0600

    added magrittr and here packages to environment setup

--- src/01_workspace_setup.R
+++ src/01_workspace_setup.R
@@ -1,9 +1,11 @@
-# clear the workspace
-rm(list = ls())
-# ensure workspace is clear
-ls()
+# load magrittr for piping functionality beyond %>% 
+library(magrittr)
+
+# load here for easy pathing to support updated file structure
+library(here)
 
 # load tidyverse
 library(tidyverse)
+
 # load stringr for string manipulation
 library(stringr)
