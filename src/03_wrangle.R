5c7f384222128053b69f67c4f751b8f4c6019b6b
Jesse Mostipak <jmostipak@teachingtrust.org>
2018-03-07 11:29:26 -0600

    initial commit

---
+++ src/03_wrangle.R
@@ -0,0 +1,8 @@
+# need to determine which columns from CAMPSTAAR2 (now achievement) we want to keep
+# work in favor of keeping more rather than cutting too severely
+
+# columns of interest:
+
+achievement %<>%
+  select(CAMPUS,
+         )
